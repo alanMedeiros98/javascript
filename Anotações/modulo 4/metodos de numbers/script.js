60c5e8cf51c9650e6fca2fe6b42620b632b2623d
alan duarte de medeiros <alantuba2012@gmail.com>
2022-01-05 22:10:43 -0300

    atualizacao dia 05/01/2022

---
+++ Anotações/modulo 4/metodos de numbers/script.js
@@ -0,0 +1,18 @@
+let n = "10.4541321854";
+
+//let res = n;
+
+//let res = n.toString();
+
+//let res = n.toFixed(2);
+
+//let res = parseInt(n);
+
+let res = parseFloat(n);
+
+console.log(res);
+
+//n.toString() = ele vai transformar o numero em uma string
+//n.toFixed() = ele vai pegar a quantidade de numeros decimais que voce especificar
+//parseInt(n) = ele vai transformar uma string em um Number
+//parseFloat(n) = ele vai transformar uma string em um Number mas vai preservar as decimais
---
+++ Anotações/modulo 4/metodos de string pt 3/script.js
@@ -0,0 +1,27 @@
+let nome = "Alan Duarte de Medeiros";
+
+//let resultado = nome;
+
+//let resultado = nome.replace('Alan', 'joao');
+
+//let resultado = nome.toUpperCase();
+
+//let resultado = nome.toLowerCase();
+
+//let resultado = nome.concat(" concatenando");
+
+//let resultado = nome.trim();
+
+//let resultado = nome.charAt(7);
+
+let resultado = nome.split(" ");
+
+console.log("Resultado: ", resultado);
+
+//nome.replace() = ele recebe 2 parâmetros, 1: a string que voce quer substituir e, 2: a string que voce quer colocar no lugar
+//nome.toUpperCase() = ele vai transformar toda a sua string em maiúscula.
+//nome.toLowerCase() = ele vai transformar toda a sua string em minuscula.
+//nome.concat() = ele vai pegar o valor da string e vai concatenar com o valor que voce passar (MAIS INÚTIL IMPOSSÍVEL!!)
+//nome.trim() = ele vai limpar todos os espaços em branco.
+//nome.charAt() = ele vai mostrar que caractere esta no posição que voce quer
+//nome.split() = ele transforma a sua string em um ARRAY
