76c4d713f4015b3c59077ae546a1308bb0e21bd7
alan duarte de medeiros <alantuba2012@gmail.com>
2022-01-25 23:30:53 -0300

    atualizacao dia 25/01/2022

---
+++ Anotações/modulo 4/intervalos pt 1/script.js
@@ -0,0 +1,17 @@
+let timer = '';
+function comecar(){
+   timer = setInterval(showTime, 1000);
+}
+function terminar(){
+    clearInterval(timer);
+}
+function showTime(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = h+':'+m+':'+s;
+
+    document.querySelector('.demo').innerHTML = txt;
+}
+
