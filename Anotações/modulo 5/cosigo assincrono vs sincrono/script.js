c065abb9a815bbd99f396709f077654c95d76f96
alan duarte de medeiros <alantuba2012@gmail.com>
2022-02-02 22:19:10 -0300

    atualizacao dia 02/02/2022

---
+++ Anotações/modulo 5/cosigo assincrono vs sincrono/script.js
@@ -0,0 +1,12 @@
+//código síncrono
+let nome = 'alan';
+let sobrenome = 'duarte';
+let completo = `${nome} ${sobrenome}`;
+//um código sincrono executa o codigo linha por linha
+
+//código assincrono
+let nome = 'alan';
+let sobrenome = 'duarte';
+let temperatura = maquininha.pegarTemperatura(); //assincrono
+let completo = `${nome} ${sobrenome}`;
+//ele não vai esperar a execução da requisição para continuar o código
